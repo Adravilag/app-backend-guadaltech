ec55eb12d5ba45001c08c7954861c78fd935d297
adravilag <adrian.davilaguerra@gmail.com>
2021-05-13 19:40:25 +0200

    API REST preparada

--- controllers/personas.js
+++ controllers/personas.js
@@ -28,15 +28,99 @@ const getPersonas = async(req, res) => {
 }
 
 const newPersona = async(req, res) => {
-    console.log('New Persona');
+
+    try {
+
+        const persona = new Persona(req.body);
+
+        await persona.save();
+
+        res.json({
+            ok: true,
+            persona
+        });
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado... revisar logs'
+        });
+
+    }
+
 }
 
 const updatePersona = async(req, res) => {
-    console.log('Update Persona');
+
+    const id = req.params.id;
+    const campos = req.body;
+
+    try {
+
+        const personaDB = await Persona.findById(id);
+
+        if (!personaDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: "No existe una persona con ese id"
+            })
+        }
+
+        const personaActualizada = await Persona.findByIdAndUpdate(id, campos, { new: true });
+
+        console.log(personaActualizada);
+
+        res.json({
+            ok: true,
+            personaActualizada
+        });
+
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).json({
+            ok: false,
+            status,
+            msg: 'Error inesperado'
+        });
+    }
+
 }
 
 const deletePersona = async(req, res) => {
-    console.log('Delete Persona');
+
+    const id = req.params.id;
+
+    try {
+
+        const persona = await Persona.findByIdAndRemove(id);
+
+        if (!persona) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe una persona con ese ID'
+            });
+        }
+
+        res.json({
+            ok: true,
+            msg: 'Persona eliminada con éxito'
+        });
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error al eliminar'
+        });
+
+    }
+
 }
 
 module.exports = { getPersonas, newPersona, updatePersona, deletePersona }
